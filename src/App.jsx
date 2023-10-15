40f0951133dfd3ff3b90fd64381442400991f49a
prakashuu <prakasamb2021@gmail.com>
2023-10-15 12:06:37 +0530

    usestate basics

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  return <Incrementor />;
 }
 
-export default App
+export default App;
+//this wont work as we expected --usestate
+// const Incrementor = () => {
+//   let count = 0;
+
+//   const clickHandler = () => {
+//     count = count + 1;
+//     console.log(count);
+//   };
+
+//   return (
+//     <div>
+//       <h1>{count}</h1>
+//       <button type="button" onClick={clickHandler}>
+//         click me to increase
+//       </button>
+//     </div>
+//   );
+// };
+
+//this works as we epected --use state
+const Incrementor = () => {
+  const [count, setCount] = useState(0);
+
+  const clickHandler = () => {
+    setCount(count + 1);
+  };
+
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button type="button" onClick={clickHandler}>
+        click me to increase
+      </button>
+    </div>
+  );
+};
